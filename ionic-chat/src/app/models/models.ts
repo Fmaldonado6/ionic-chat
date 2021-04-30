1188ffaaf9b9e02b1b3a92fde306548ba24952ac
Fernando Maldonado <fmaldonado824@gmail.com>
2021-04-30 10:57:45 -0500

    se agregan modelos de datos

---
+++ ionic-chat/src/app/models/models.ts
@@ -0,0 +1,44 @@
+export class User {
+    _id: string = ""
+    username: string = ""
+    email: string = ""
+    password: string = ""
+    get id() { return this._id }
+    set id(val: string) { this._id = val }
+}
+
+export class Chat {
+    _id: string = ""
+    chatName: string = ""
+    participants: string[] = []
+    get id() { return this._id }
+    set id(val: string) { this._id = val }
+}
+
+export class WebsocketMessage {
+    type: number = WebsocketMessageTypes.connection
+    message: string = ""
+}
+
+export class Message {
+    _id: string = ""
+    senderName: string = ""
+    senderId: string = ""
+    receiverId: string = ""
+    chatId: string = ""
+    message: string = ""
+    messageType: number = MessageType.text
+
+    get id() { return this._id }
+    set id(val: string) { this._id = val }
+}
+
+export enum MessageType {
+    text,
+    image
+}
+
+export enum WebsocketMessageTypes {
+    connection,
+    send
+}
